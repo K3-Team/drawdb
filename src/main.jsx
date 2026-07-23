9648d485fdafe0652a1a42a2ddb4ca83c8819321
id3v1669 <git@id3v1669.com>
2026-07-23 14:31:04 +0800

    fix: self-host Monaco and icon fonts, add Content-Security-Policy

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { LocaleProvider } from "@douyinfe/semi-ui";
+import * as monaco from "monaco-editor";
+import { loader } from "@monaco-editor/react";
 import App from "./App.jsx";
 import en_US from "@douyinfe/semi-ui/lib/es/locale/source/en_US";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
 import "./i18n/i18n.js";
+
+// Use the locally bundled monaco-editor instead of fetching it from a CDN
+// at runtime. Must run before any <Editor>/<DiffEditor> mounts.
+loader.config({ monaco });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
